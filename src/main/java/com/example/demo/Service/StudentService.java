39334fc75001c75a9ce668ceaf60932ca1b55374
svisan <svisan@grubhub.com>
2024-07-19 12:52:52 +0300

    added plugin for spotless

--- src/main/java/com/example/demo/Service/StudentService.java
+++ src/main/java/com/example/demo/Service/StudentService.java
@@ -1,59 +1,49 @@
 package com.example.demo.Service;
 
-import ch.qos.logback.core.pattern.parser.OptionTokenizer;
 import com.example.demo.StudentModel.Student;
-import org.springframework.stereotype.Service;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
+import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
-    private List<Student> students = new ArrayList<>();
+  private List<Student> students = new ArrayList<>();
 
-    public StudentService() {
-        students.add(new Student(1L, "Tudor", 9.80));
-        students.add(new Student(2L, "Visan", 9.60));
-        students.add(new Student(3L, "Grancea", 9.45));
-        students.add(new Student(4L, "Mihaescu", 9.10));
+  public StudentService() {
+    students.add(new Student(1L, "Tudor", 9.80));
+    students.add(new Student(2L, "Visan", 9.60));
+    students.add(new Student(3L, "Grancea", 9.45));
+    students.add(new Student(4L, "Mihaescu", 9.10));
+  }
 
+  public List<Student> getAllStudents() {
+
+    return students;
+  }
+
+  public Optional<Student> getStudentById(Long id) {
+    for (Student student : students) {
+      if (student.getId().equals(id)) {
+        return Optional.of(student);
+      }
     }
 
+    return Optional.empty();
+  }
 
-    public List<Student> getAllStudents() {
+  public Student addStudent(Student student) {
+    students.add(student);
+    return student;
+  }
 
-        return students;
-    }
+  public Optional<Student> deleteStudent(Long id) {
+    Optional<Student> studentOpt = getStudentById(id);
+    studentOpt.ifPresent(students::remove);
+    return studentOpt;
+  }
 
-    public Optional<Student> getStudentById(Long id) {
-        for (Student student : students) {
-            if (student.getId().equals(id)) {
-                return Optional.of(student);
-            }
-        }
-
-        return Optional.empty();
-    }
-
-    public Student addStudent(Student student) {
-        students.add(student);
-        return student;
-
-    }
-
-    public Optional<Student> deleteStudent(Long id) {
-        Optional<Student> studentOpt = getStudentById(id);
-        studentOpt.ifPresent(students::remove);
-        return studentOpt;
-    }
-
-    public void deleteAll() {
-        students.clear();
-    }
+  public void deleteAll() {
+    students.clear();
+  }
 }
-
-
